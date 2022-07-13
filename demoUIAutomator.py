428adca82ea94304ebd3887677dbd3d56bd3e4f8
RFNshare <rfnshare@gmail.com>
2022-07-13 18:53:46 +0600

    Basic UIAutomator locator ideas 2

--- demoUIAutomator.py
+++ demoUIAutomator.py
@@ -11,4 +11,6 @@ driver.implicitly_wait(10)
 driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
 # driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Animation\")").click()
 a = driver.find_elements(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().clickable(true)')  # Find by property value
+for i in a:
+    print(i.get_attribute('text'))
 print(len(a))
