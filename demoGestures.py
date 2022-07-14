ce5610ad34ebc09ea653016c1efb796c7f5d4eac
RFNshare <rfnshare@gmail.com>
2022-07-14 16:46:08 +0600

    Longpres Perform With TouchAction & Validate Toast Message using Wait

--- demoGestures.py
+++ demoGestures.py
@@ -2,7 +2,8 @@ import time
 
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.common.touch_action import TouchAction
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from LaunchApp import capabilities
 
 driver = capabilities()
@@ -10,7 +11,17 @@ driver.implicitly_wait(20)
 driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
 driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'text("Expandable Lists")').click()
 # Tapping
-element = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "1. Custom Adapter")
 touch = TouchAction(driver)
+element = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "1. Custom Adapter")
 touch.tap(element).perform()
+time.sleep(5)
+ppl_name = driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text='People Names']")
+touch.long_press(ppl_name).perform()
+time.sleep(2)
+driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@text ='Sample action']").click()
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((AppiumBy.XPATH, "//android.widget.Toast")))
+text = driver.find_element(AppiumBy.XPATH, "//android.widget.Toast").text
+print(text)
+assert "People Names: Group 0" in text
 time.sleep(5)
