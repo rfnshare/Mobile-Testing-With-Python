3261b0238e47d70a99317e3659cab8c2c15093e5
RFNshare <rfnshare@gmail.com>
2022-07-16 15:11:56 +0600

    Update with auto start & stop appium server

--- LaunchApp.py
+++ LaunchApp.py
@@ -1,7 +1,9 @@
+import time
+
 from Tools.scripts.win_add2path import PATH
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
-
+from appium.webdriver.appium_service import AppiumService
 
 # Deprecated
 # desired_caps = {
@@ -11,14 +13,19 @@ from appium.options.android import UiAutomator2Options
 #         "appium:uid": "emulator-5554",
 #         "automationName": "UIAutomator2",
 #     }
+
+service = AppiumService()
+
+
 def capabilities():
+    service.start()
     options = UiAutomator2Options()
     options.device_name = 'Pixel 4 API 30'
-    # options.app = 'C:/Users/rfnsh/PycharmProjects/pythonMobileTesting/resources/Maps_base.apk'
+    options.app = 'C:/Users/rfnsh/PycharmProjects/pythonMobileTesting/resources/ApiDemos-debug.apk'
     # options.platformVersion = '11'
     # options.udid = 'emulator-5554'
-    options.app_package = 'io.appium.android.apis'
-    options.app_activity = 'io.appium.android.apis.ApiDemos'
+    # options.app_package = 'io.appium.android.apis'
+    # options.app_activity = 'io.appium.android.apis.ApiDemos'
 
     driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
     return driver
@@ -27,4 +34,6 @@ def capabilities():
 if __name__ == '__main__':
     driver = capabilities()
     driver.implicitly_wait(20)
+    time.sleep(5)
     driver.quit()
+    service.stop()
