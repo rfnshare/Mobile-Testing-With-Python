d65217e235eb8a808a14401aebdfb6a64fc2c013
RFNshare <rfnshare@gmail.com>
2022-07-16 19:55:45 +0600

    Update Demo Basic Script with dynamic server stopping

--- demoBasic.py
+++ demoBasic.py
@@ -1,9 +1,10 @@
 import time
 
+from appium.webdriver.appium_service import AppiumService
 from appium.webdriver.common.appiumby import AppiumBy
 
 from LaunchApp import capabilities
-
+service = AppiumService()
 driver = capabilities()
 driver.find_element(AppiumBy.CSS_SELECTOR, "android.widget.TextView[content-desc='Preference']").click()
 driver.find_element(AppiumBy.ACCESSIBILITY_ID, "3. Preference dependencies").click()
@@ -16,5 +17,6 @@ driver.find_element(AppiumBy.ID, "android:id/checkbox").click()
 driver.find_element(AppiumBy.XPATH, "(//android.widget.TextView[@resource-id='android:id/title'])[3]").click()
 driver.find_element(AppiumBy.ID, "android:id/edit").send_keys("Sample_WiFi")
 driver.find_element(AppiumBy.ID, "android:id/button1").click()
-# time.sleep(5)
-# driver.quit()
+time.sleep(5)
+driver.quit()
+service.stop()
