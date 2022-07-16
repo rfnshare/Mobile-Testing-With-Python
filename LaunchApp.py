89d3c90bc54456ef7f6b77e0204eba2752ebc9ef
RFNshare <rfnshare@gmail.com>
2022-07-16 08:57:42 +0600

    Update App launch methods with new Appium server version 2.0 Beta

--- LaunchApp.py
+++ LaunchApp.py
@@ -1,26 +1,29 @@
-import time
-
+from Tools.scripts.win_add2path import PATH
 from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
+from appium.options.android import UiAutomator2Options
 
 
-# desired_caps['Sample'] = 'SampleValue'
-
-# driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/EnterValue").click()
-# time.sleep(2)
-# driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/Et1").send_keys("10")
-# driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/Btn1").click()
-# ok = driver.find_element(AppiumBy.ID, "com.code2lead.kwad:id/Tv1").text
-# print(ok)
-
+# Deprecated
+# desired_caps = {
+#         "appium:appPackage": "io.appium.android.apis",
+#         "appium:appActivity": "io.appium.android.apis.ApiDemos",
+#         "platformName": "Android",
+#         "appium:uid": "emulator-5554",
+#         "automationName": "UIAutomator2",
+#     }
 def capabilities():
-    desired_caps = {
-        "appium:appPackage": "io.appium.android.apis",
-        "appium:appActivity": "io.appium.android.apis.ApiDemos",
-        "platformName": "Android",
-        "appium:uid": "emulator-5554",
-        "automationName": "UIAutomator2",
-    }
+    options = UiAutomator2Options()
+    options.device_name = 'Pixel 4 API 30'
+    options.app = 'C:/Users/rfnsh/PycharmProjects/pythonMobileTesting/resources/ApiDemos-debug.apk'
+    # options.platformVersion = '11'
+    # options.udid = 'emulator-5554'
+    # options.app_package = 'io.appium.android.apis'
+    # options.app_activity = 'io.appium.android.apis.ApiDemos'
 
-    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+    driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
     return driver
+
+
+if __name__ == '__main__':
+    driver = capabilities()
+    driver.implicitly_wait(20)
