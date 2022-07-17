795f76e8b61b916d08f2d7d3b9b71195da5fa7a5
RFNshare <rfnshare@gmail.com>
2022-07-17 17:21:23 +0600

    Updated launch app code with class & functions demobasic codes

--- demoBasic.py
+++ demoBasic.py
@@ -1,22 +1,26 @@
 import time
+import unittest
 
 from appium.webdriver.appium_service import AppiumService
 from appium.webdriver.common.appiumby import AppiumBy
-from LaunchApp import launchApp
+from LaunchApp import ConfigureAppium
 
-service = AppiumService()
-driver = launchApp()
-driver.find_element(AppiumBy.CSS_SELECTOR, "android.widget.TextView[content-desc='Preference']").click()
-driver.find_element(AppiumBy.ACCESSIBILITY_ID, "3. Preference dependencies").click()
-driver.find_element(AppiumBy.ID, "android:id/checkbox").click()
-# wifi_settings = driver.find_elements(AppiumBy.XPATH, "//android.widget.TextView[@resource-id='android:id/title']")
-# print(len(wifi_settings))
-# for i in wifi_settings:
-#     print(i.get_attribute("text"))
-# wifi_settings[2].click()
 
-driver.find_element(AppiumBy.XPATH, "(//android.widget.TextView[@resource-id='android:id/title'])[3]").click()
-driver.find_element(AppiumBy.ID, "android:id/edit").send_keys("Sample_WiFi")
-driver.find_element(AppiumBy.ID, "android:id/button1").click()
-time.sleep(5)
-driver.quit()
+class DemoBasic(ConfigureAppium, unittest.TestCase):
+    driver = ConfigureAppium().launchApp()
+
+    def test_basic(self):
+        self.driver.find_element(AppiumBy.CSS_SELECTOR, "android.widget.TextView[content-desc='Preference']").click()
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "3. Preference dependencies").click()
+        self.driver.find_element(AppiumBy.ID, "android:id/checkbox").click()
+        # wifi_settings = self.driver.find_elements(AppiumBy.XPATH, "//android.widget.TextView[@resource-id='android:id/title']")
+        # print(len(wifi_settings))
+        # for i in wifi_settings:
+        #     print(i.get_attribute("text"))
+        # wifi_settings[2].click()
+
+        self.driver.find_element(AppiumBy.XPATH,
+                                 "(//android.widget.TextView[@resource-id='android:id/title'])[3]").click()
+        self.driver.find_element(AppiumBy.ID, "android:id/edit").send_keys("Sample_WiFi")
+        self.driver.find_element(AppiumBy.ID, "android:id/button1").click()
+        self.driver.quit()
