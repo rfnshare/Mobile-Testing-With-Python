992792dd27a86d22367ef7951d3942ea1d6c0412
RFNshare <rfnshare@gmail.com>
2022-07-17 14:30:13 +0600

    Update Capabilities with many options

--- LaunchApp.py
+++ LaunchApp.py
@@ -15,19 +15,37 @@ from appium.webdriver.appium_service import AppiumService
 #     }
 
 service = AppiumService()
+options = UiAutomator2Options()
 
 
 def capabilities():
     service.start()
-    options = UiAutomator2Options()
     options.device_name = 'Pixel 4 API 30'
+    options.udid = 'emulator-5554'
     options.app = 'C:/Users/rfnsh/PycharmProjects/pythonMobileTesting/resources/ApiDemos-debug.apk'
-    # options.platformVersion = '11'
-    # options.udid = 'emulator-5554'
-    # options.app_package = 'io.appium.android.apis'
-    # options.app_activity = 'io.appium.android.apis.ApiDemos'
+    options.platformVersion = '11'
+    options.app_package = 'io.appium.android.apis'
+    options.app_activity = 'io.appium.android.apis.ApiDemos'
+    # Enforces the server to dump the actual XML page source into the log if any error happens.
+    options.print_page_source_on_find_failure = True
+    # Skip the UiAutomator2 Server component installation on the device under test and all the related checks
+    options.skip_server_installation = True
+    # Device startup checks (whether it is ready and whether Settings app is installed) will be canceled.
+    options.skip_device_initialization = True
+    # Whether to grant all the requested application permissions automatically when a test starts
+    options.auto_grant_permissions = True
+    # If set to true then emulator starts in headless mode (e.g. no UI is shown).
+    # options.is_headless = True
+    # Unlocking Phone
+    # options.unlock_strategy = 'locksettings'
+    # options.unlock_type = 'pin'
+    # options.unlock_key = '1234'
+
+
 
     driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+    session = driver.session_id
+    print(session)
     return driver
 
 
