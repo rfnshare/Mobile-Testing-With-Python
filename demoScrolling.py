27c9076e67bdbefdb87f8612a7d9a7f36557161f
RFNshare <rfnshare@gmail.com>
2022-07-18 14:44:34 +0600

    Scrolling Updated

--- demoScrolling.py
+++ demoScrolling.py
@@ -1,14 +1,17 @@
 import time
+import unittest
 
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from LaunchApp import capabilities
+from LaunchApp import ConfigureAppium
 
-driver = capabilities()
-driver.implicitly_wait(20)
-driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
-origin_el = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Animation")
-destination_el = driver.find_element(AppiumBy.ACCESSIBILITY_ID, "WebView")
-driver.scroll(origin_el, destination_el)
+
+class DemoScrollGesture(ConfigureAppium, unittest.TestCase):
+    driver = ConfigureAppium().launchApp()
+    def test_basicScrollGesture(self):
+        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
+        origin_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Animation")
+        destination_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "WebView")
+        self.driver.execute_script('mobile: scrollGesture', {'elementId': destination_el, 'direction': 'down', 'percent': 3.0})
