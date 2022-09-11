7664e2991fd3f0e5f4c2a6b636062cd1c7fc51f9
RFNshare <rfnshare@gmail.com>
2022-09-11 16:35:55 +0600

    Scrolling Updated With Destination Element

--- demoScrolling.py
+++ demoScrolling.py
@@ -15,31 +15,31 @@ class DemoScrollGesture(ConfigureAppium, unittest.TestCase):
         self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
                                  f"new UiScrollable(new UiSelector()).scrollIntoView(text(\"{txt}\"))")
 
-    def can_scroll_more(self):
+    def scroll_more(self):
         can_scroll_more = self.driver.execute_script('mobile: scrollGesture', {
             'left': 100, 'top': 500, 'width': 900, 'height': 500,
             'direction': 'down',
             'percent': 3.0
         })
-        return can_scroll_more
+        while can_scroll_more is True:
+            can_scroll_more = self.driver.execute_script('mobile: scrollGesture', {
+                'left': 100, 'top': 500, 'width': 900, 'height': 500,
+                'direction': 'down',
+                'percent': 3.0
+            })
+            print(can_scroll_more)
 
     def test_basicScrollGesture(self):
         self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
 
         # Google Built In Scrolling
-        self.scroll('WebView')
+        # self.scroll('WebView')
 
         # Scroll Gesture (Until End)
-        # self.can_scroll_more()
-        # while self.can_scroll_more() is True:
-        #     self.can_scroll_more()
+        # self.scroll_more()
 
         # origin_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Animation")
-        # destination_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "WebView")
+        destination_el = self.driver.find_element(AppiumBy.ID, "android:id/list")
 
-        # destination_el = self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[
-        # @resource-id='com.karshare.app:id/car']")
-
-        # # self.driver.scroll(origin_el, destination_el)
-        # self.driver.execute_script('mobile: scrollGesture',
-        #                            {'elementId': destination_el, 'direction': 'down', 'percent': 10.0})
+        self.driver.execute_script('mobile: scrollGesture',
+                                   {'elementId': destination_el, 'direction': 'down', 'percent': 3.0})
