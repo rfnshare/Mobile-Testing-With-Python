9479a440c8cf2b685a164dcc1b45b1039e9f215d
RFNshare <rfnshare@gmail.com>
2022-09-11 17:59:41 +0600

    Scrolling Gesture Added in Karshare App

---
+++ LaunchKarshareApp.py
@@ -0,0 +1,54 @@
+import time
+
+from Tools.scripts.win_add2path import PATH
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
+
+
+# Deprecated
+# desired_caps = {
+#         "appium:appPackage": "io.appium.android.apis",
+#         "appium:appActivity": "io.appium.android.apis.ApiDemos",
+#         "platformName": "Android",
+#         "appium:uid": "emulator-5554",
+#         "automationName": "UIAutomator2",
+#     }
+
+
+class ConfigureAppium:
+    service = AppiumService()
+    options = UiAutomator2Options()
+    driver = None
+
+    def launchApp(self):
+        self.service.start()
+        self.options.avd = 'Pixel_4_API_30'
+        # self.options.udid = '8RBDU19325003729'
+        self.options.platformVersion = '10'
+        self.options.app_package = 'com.karshare.app'
+        self.options.app_activity = 'com.karshare.app.MainActivity'
+        # Enforces the server to dump the actual XML page source into the log if any error happens.
+        self.options.print_page_source_on_find_failure = True
+        # Skip the UiAutomator2 Server component installation on the device under test and all the related checks
+        # self.options.skip_server_installation = True
+        # Device startup checks (whether it is ready and whether Settings app is installed) will be canceled.
+        # self.options.skip_device_initialization = True
+        # Whether to grant all the requested application permissions automatically when a test starts
+        self.options.auto_grant_permissions = True
+        # If set to true then emulator starts in headless mode (e.g. no UI is shown).
+        self.options.is_headless = True
+        # Unlocking Phone
+        # options.unlock_strategy = 'locksettings'
+        # options.unlock_type = 'pin'
+        # options.unlock_key = '1234'
+
+        driver = webdriver.Remote("http://127.0.0.1:4723", options=self.options)
+        session = driver.session_id
+        print(session)
+        driver.implicitly_wait(10)
+        return driver
+
+
+if __name__ == '__main__':
+    pass
---
+++ demoKarshareScrolling.py
@@ -0,0 +1,31 @@
+import time
+import unittest
+
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from LaunchKarshareApp import ConfigureAppium
+
+
+class DemoScrollGesture(ConfigureAppium, unittest.TestCase):
+    driver = ConfigureAppium().launchApp()
+
+    def scroll(self, txt):
+        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+                                 f"new UiScrollable(new UiSelector()).scrollIntoView(text(\"{txt}\"))")
+
+    def test_basicScrollGesture(self):
+        time.sleep(30)
+        destination_element = self.driver.find_element(AppiumBy.ID, "com.karshare.app:id/results")
+
+        scroll_more = self.driver.execute_script('mobile: scrollGesture',
+                                                 {'elementId': destination_element, 'direction': 'down',
+                                                  'percent': 3.0})
+        print("---->", scroll_more)
+
+        while scroll_more is True or False:
+            scroll_more = self.driver.execute_script('mobile: scrollGesture',
+                                                     {'elementId': destination_element, 'direction': 'down',
+                                                      'percent': 3.0})
+            print("----<", scroll_more)
