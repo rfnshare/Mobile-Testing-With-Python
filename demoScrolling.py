72bdad3e8f69c8df7dd1cc78cea03773ece4b9e9
RFNshare <rfnshare@gmail.com>
2022-09-11 15:50:20 +0600

    Scrolling Updated

--- demoScrolling.py
+++ demoScrolling.py
@@ -11,27 +11,35 @@ from LaunchApp import ConfigureAppium
 class DemoScrollGesture(ConfigureAppium, unittest.TestCase):
     driver = ConfigureAppium().launchApp()
 
+    def scroll(self, txt):
+        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
+                                 f"new UiScrollable(new UiSelector()).scrollIntoView(text(\"{txt}\"))")
+
+    def can_scroll_more(self):
+        can_scroll_more = self.driver.execute_script('mobile: scrollGesture', {
+            'left': 100, 'top': 500, 'width': 900, 'height': 500,
+            'direction': 'down',
+            'percent': 3.0
+        })
+        return can_scroll_more
+
     def test_basicScrollGesture(self):
         self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "text(\"Views\")").click()
-        # can_scroll_more = self.driver.execute_script('mobile: scrollGesture', {
-        #     'left': 100, 'top': 100, 'width': 200, 'height': 200,
-        #     'direction': 'down',
-        #     'percent': 3.0
-        # })
-        # print(can_scroll_more)
-        # while can_scroll_more is True:
-        #     can_scroll_more = self.driver.execute_script('mobile: scrollGesture', {
-        #         'left': 100, 'top': 100, 'width': 200, 'height': 200,
-        #         'direction': 'down',
-        #         'percent': 3.0
-        #     })
-        #     print(can_scroll_more)
 
-        # self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-        #                          'new UiScrollable(new UiSelector()).scrollIntoView(text(\"ImageView\"))')
-        origin_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Animation")
-        destination_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "WebView")
-        destination_el = self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@resource-id='com.karshare.app:id/car']")
-        # self.driver.scroll(origin_el, destination_el)
-        self.driver.execute_script('mobile: scrollGesture',
-                                   {'elementId': destination_el, 'direction': 'down', 'percent': 10.0})
+        # Google Built In Scrolling
+        # self.scroll('WebView')
+
+        # Scroll Gesture (Until End)
+        self.can_scroll_more()
+        while self.can_scroll_more() is True:
+            self.can_scroll_more()
+
+        # origin_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Animation")
+        # destination_el = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "WebView")
+
+        # destination_el = self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[
+        # @resource-id='com.karshare.app:id/car']")
+
+        # # self.driver.scroll(origin_el, destination_el)
+        # self.driver.execute_script('mobile: scrollGesture',
+        #                            {'elementId': destination_el, 'direction': 'down', 'percent': 10.0})
